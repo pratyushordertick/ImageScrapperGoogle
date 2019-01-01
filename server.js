892d9e8883aeeb77f9ef3a219465226fa532b020
pratyush <pratyush@ordertick.com>
2019-01-01 15:18:10 +0530

    clean second commit

--- server.js
+++ server.js
@@ -4,11 +4,16 @@ var config = require('./config/config.json')
 var app = express();
 var mongoURI = config.development.MONGO_URI;
 mongoose.Promise = global.Promise;
-app.get('/',(req,res)=>{
-    res.send('hello')
-});
+// app.get('/',(req,res)=>{
+//     res.send('hello')
+// });
 mongoose.connect(mongoURI, function(err) {
     if (!err) { console.log('connection ok  successful'); } else { console.log(err) }
+})
+
+app.use(express.static(__dirname + '/client/dist/client'))
+app.use(function (req, res) {
+    res.sendFile(__dirname + '/client/dist/client/index.html')
 })
 
 app.listen(9090,()=>{
